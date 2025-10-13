f22f5da1abdce5d7df2595fcda0bb8a74a65b0c2
ThaoVu <thaovu.dev17@gmail.com>
2025-10-13 22:51:08 +0700

    update

--- resources/js/Pages/Admin/ListUser.vue
+++ resources/js/Pages/Admin/ListUser.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "vue";
 import { defineProps } from "vue";
-import AdminLayout from "@Layouts/AdminLayout.vue";
+import AdminLayout from "../../Layouts/AdminLayout.vue";
 
 const { users } = defineProps({
     users: Array,
