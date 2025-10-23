abbb39825601baf59ab391bc043b196f0b23200c
ThaoVu <thaovu.dev17@gmail.com>
2025-10-24 00:04:12 +0700

    UPdate

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,10 +2,22 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-g">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>GEARNOW</title>
+    <meta name="description"
+        content="GEARNOW - Cửa hàng chuyên cung cấp các thiết bị, phụ kiện và công nghệ mới nhất dành cho bạn.">
+    <meta name="keywords" content="thiết bị, công nghệ, phụ kiện, đồ công nghệ, sản phẩm mới, gear, gadgets, accessories">
+    <meta name="author" content="GEARNOW">
+    <meta property="og:title" content="GEARNOW - Cửa hàng công nghệ và phụ kiện hiện đại">
+    <meta property="og:description"
+        content="Khám phá những sản phẩm công nghệ, phụ kiện và đồ chơi công nghệ mới nhất tại GEARNOW.">
+    <meta property="og:image" content="{{ asset('images/og-image.jpg') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:type" content="website">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/favicon.png') }}">
+    <title>GEARNOW - Thiết bị & Phụ kiện công nghệ</title>
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
 </head>
